a00a30312c0e87d23f097f95de6116e39abc877e
Lizzie Q <lizzieqtech@gmail.com>
2022-03-04 16:29:55 -0600

    salaryCalculator

---
+++ salaryCalculator.js
@@ -0,0 +1,37 @@
+console.log('JS');
+
+/*$(onReady).click(function)*/
+
+/*$(onReady);   //    Check which is best for future use of JQuery - above or below "on Ready JQ"
+function onReady (){
+}*/
+
+//To DO: variable for my form(s) / tables of data of employees
+// get input values
+//storing on click 
+//log variables to console
+//take salary input to add to 
+
+$(document).ready(onReady);{
+//function onReady(){
+  //salaryCalculator
+}
+
+// view notes - can I put tables into JS easily?
+let nameLater = 
+<form class ='form' id="inputForm">      
+<table>
+  <thead>
+    <tr>
+
+<td></td>
+
+
+
+    </tr>
+
+  </thead>
+
+</table>
+
+</form>
