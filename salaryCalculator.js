db9f97ad95e130b307db5671e461e754da3ac241
Lizzie Q <lquaidoo@gmail.com>
2022-03-05 15:18:56 -0600

    FinallyIndexJqueryData

--- salaryCalculator.js
+++ salaryCalculator.js
@@ -6,14 +6,20 @@ console.log('JS');
 function onReady (){
 }*/
 
-//To DO: variable for my form(s) / tables of data of employees
-// get input values for JQuery (ask team ... ) put in html then call in class & ids js
+//To DO: variable for my form(s) / tables of data of employees IN INDEX
+// get input values for JQuery (ask team ... ) put in html then JS 
 //storing on click 
 //log variables to console
 //take salary input to add to 
 
-$(document).ready(onReady);{
-//function onReady(){
-  //salaryCalculator
-}
+//NEXT STEPS:
+// 1.) click EVENT listener  submit/delete function readyNow ()
+// grab index employee data function add emploees ()
+// append employees and append delete button give an id 
+// 
 
+$(document).ready(onReady);{
+  
+  //function onReady(){
+    //salaryCalculator
+  }
